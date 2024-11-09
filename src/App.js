42b38ee55419fd81f2ab8b88841f4d147f7060ba
dlsxjzld <sinjuk1@naver.com>
2024-11-09 17:09:28 +0900

    feat(App.js): product, promotion 파일 읽기 추가해

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { InputView } from './View/InputView.js';
+
+const PRODUCT_FILENAME = 'products.md';
+const PROMOTION_FILENAME = 'promotions.md';
 class App {
-  async run() {}
+  async run() {
+    const productsInput = InputView.readFile(PRODUCT_FILENAME);
+    const promotionsInput = InputView.readFile(PROMOTION_FILENAME);
+  }
 }
 
 export default App;

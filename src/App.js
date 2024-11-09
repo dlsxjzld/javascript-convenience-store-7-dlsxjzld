c6b2a4969463b5a139a4316af3ff2f1e5b0569a3
dlsxjzld <sinjuk1@naver.com>
2024-11-09 19:15:18 +0900

    docs(README.md): 상품 목록 모델로 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import ProductList from './Model/ProductList.js';
+import PromotionList from './Model/PromotionList.js';
 import { InputView } from './View/InputView.js';
 import { PRODUCT_FILENAME, PROMOTION_FILENAME } from './constants.js';
+
 class App {
   async run() {
     const productsInput = InputView.readFile(PRODUCT_FILENAME);
     const promotionsInput = InputView.readFile(PROMOTION_FILENAME);
+    const productList = new ProductList(productsInput);
+    const promotionList = new PromotionList(promotionsInput);
   }
 }
 

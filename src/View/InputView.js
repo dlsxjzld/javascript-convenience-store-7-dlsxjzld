7202cedf2163efe27fa3935a7915725d8554c62e
dlsxjzld <sinjuk1@naver.com>
2024-11-09 17:09:05 +0900

    feat(View/InputView.js): md파일 읽기 추가해

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,6 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
+import fs from 'fs';
+import path from 'path';
 
-const InputView = {
+export const InputView = {
+  readFile(fileName) {
+    const absoluteDirectoryPath = path.resolve();
+    const targetFilePath = `${absoluteDirectoryPath}/public/${fileName}`;
+
+    return fs.readFileSync(targetFilePath, 'utf8').split('\n');
+  },
+
   async readItem() {
     const input = await Console.readLineAsync(
       '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])',

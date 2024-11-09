58f7c37418c1d7f06d0ed9a0ebb5229ff2fcaf68
dlsxjzld <sinjuk1@naver.com>
2024-11-09 20:41:32 +0900

    feat(App.js): 안내문 및 재고 출력 연결해

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ProductList from './Model/ProductList.js';
 import PromotionList from './Model/PromotionList.js';
 import { InputView } from './View/InputView.js';
+import { OutputView } from './View/OutputView.js';
 import { PRODUCT_FILENAME, PROMOTION_FILENAME } from './constants.js';
 
 class App {
@@ -9,6 +10,9 @@ class App {
     const promotionsInput = InputView.readFile(PROMOTION_FILENAME);
     const productList = new ProductList(productsInput);
     const promotionList = new PromotionList(promotionsInput);
+
+    OutputView.printInstructions();
+    OutputView.printInventory(productList);
   }
 }
 

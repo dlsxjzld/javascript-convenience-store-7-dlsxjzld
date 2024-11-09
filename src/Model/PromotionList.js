6af6bed75dcbb5d789460734990c6635b2f840ee
dlsxjzld <sinjuk1@naver.com>
2024-11-09 19:14:16 +0900

    feat(Model/PromotionList.js): 프로모션 리스트 만들기

---
+++ src/Model/PromotionList.js
@@ -0,0 +1,36 @@
+export default class PromotionList {
+  #keyList;
+
+  #promotionList;
+
+  constructor(input) {
+    const [keyList, ...metaDataList] = input;
+    this.#keyList = keyList;
+    this.#promotionList = new Map();
+    this.#initialize(metaDataList);
+    this.toString();
+  }
+
+  #initialize(metaDataList) {
+    metaDataList.forEach((metaData) => {
+      const [name] = metaData;
+      const promotion = this.makePromotion(metaData);
+      this.#promotionList.set(name, promotion);
+    });
+  }
+
+  makePromotion([name, buy, get, startDate, endDate]) {
+    const promotion = {
+      name,
+      buy: Number(buy),
+      get: Number(get),
+      startDate,
+      endDate,
+    };
+    return promotion;
+  }
+
+  toString() {
+    console.log(this.#promotionList);
+  }
+}

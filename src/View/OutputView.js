4c660c494d77fd736e7ef57f4a9afc5b9e169e29
dlsxjzld <sinjuk1@naver.com>
2024-11-09 20:40:18 +0900

    feat(View/OutputView.js): 편의점 안내문 및 재고 출력해

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,7 +1,40 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const OutputView = {
+export const OutputView = {
   printProducts() {
     Console.print('- 콜라 1,000원 10개 탄산2+1');
+  },
+
+  printInstructions() {
+    Console.print('안녕하세요. W편의점입니다.');
+    Console.print('현재 보유하고 있는 상품입니다.\n');
+  },
+
+  printInventory(productList) {
+    const inventoryList = productList.getProductInventory();
+    inventoryList.forEach(({ name, price, quantity, promotion }) => {
+      this.printProduct({ name, price, quantity, promotion });
+    });
+    Console.print('');
+  },
+
+  printProduct({ name, price, quantity, promotion }) {
+    if (quantity === null || promotion === null) return;
+    Console.print(
+      `- ${name} ${price.toLocaleString()}원 ${this.getQuantity(quantity)}${this.getPromotion(promotion)}`,
+    );
+  },
+
+  getQuantity(quantity) {
+    if (quantity === 0) {
+      return '재고 없음';
+    }
+    return `${quantity}개`;
+  },
+  getPromotion(promotion) {
+    if (promotion !== 'null') {
+      return ` ${promotion}`;
+    }
+    return '';
   },
 };

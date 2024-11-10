ab0072c7b75dfda57ee7003a6d56ef4b5f2878f5
dlsxjzld <sinjuk1@naver.com>
2024-11-11 01:20:20 +0900

    feat(constants.js): 상수 모듈화

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,9 @@
 export const PRODUCT_FILENAME = 'products.md';
 export const PROMOTION_FILENAME = 'promotions.md';
+export const BUY_MESSAGE =
+  '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n';
+
+export const MEMBERSHIP_DISCOUNT_MESSAGE =
+  '멤버십 할인을 받으시겠습니까? (Y/N)\n';
+
+export const MORE_BUY = '감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n';

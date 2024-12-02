f102654dce0c2511d9155499b7405eeaf8b946d8
dlsxjzld <sinjuk1@naver.com>
2024-12-02 13:41:03 +0900

    feat: view 추가

---
+++ src/view/OutputView.js
@@ -0,0 +1,9 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export const OutputView = {
+  printProducts() {
+    Console.print('- 콜라 1,000원 10개 탄산2+1');
+    // ...
+  },
+  // ...
+};

bba57c37603247b967a26d490aeefa5c267a52d9
dlsxjzld <sinjuk1@naver.com>
2024-12-02 14:10:53 +0900

    feat: 사용자가 입력할 때 에러 발생시 재입력 받을 수 있도록 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,15 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
 import path from 'path';
 import fs from 'fs';
+import { check } from '../validation/validateFunctions.js';
 
 export const InputView = {
-  // TODO: 구현에 필요한 상품 목록과 행사 목록을 파일 입출력을 통해 불러온다.
-  //   TODO: `public/products.md`과 `public/promotions.md` 파일을 이용한다.
-  //   TODO: 두 파일 모두 내용의 형식을 유지한다면 값은 수정할 수 있다.
-  async readItem() {
-    const input = await Console.readLineAsync(
-      '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])',
-    );
+  async readUserInput(message, validation, rest) {
+    const input = await Console.readLineAsync(message);
+    if (check(input, validation, rest)) {
+      return input;
+    }
+    return this.readUserInput(message, validation, rest);
   },
 
   readFileSync(fileName) {

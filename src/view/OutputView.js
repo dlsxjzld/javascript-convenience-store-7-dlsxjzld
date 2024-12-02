6c95d1c0213e3991a771180bacf824cd493a9237
dlsxjzld <sinjuk1@naver.com>
2024-12-02 16:38:53 +0900

    feat: OutputView에서 결과 출력

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,7 +3,8 @@ import { Console } from '@woowacourse/mission-utils';
 export const OutputView = {
   printProducts() {
     Console.print('- 콜라 1,000원 10개 탄산2+1');
-    // ...
   },
-  // ...
+  printResult(result) {
+    Console.print(result);
+  },
 };

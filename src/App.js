a860eec64eab19da4cac61d5b2043079dc7d2e7f
dlsxjzld <sinjuk1@naver.com>
2024-12-02 14:11:57 +0900

    feat: 구매할 상품과 수량을 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { PRODUCTS, PROMOTIONS } from './constants.js';
+import { PRODUCTS, PROMOTIONS, READ_ITEM_MESSAGE } from './constants.js';
+import { readItem } from './validation/validateFunctions.js';
 import { InputView } from './view/InputView.js';
 
 class App {
   async run() {
     const products = InputView.readFileSync(PRODUCTS);
     const promotions = InputView.readFileSync(PROMOTIONS);
+
+    const userInput = await InputView.readUserInput(
+      READ_ITEM_MESSAGE,
+      readItem,
+    );
   }
 }
 

9b208d1b25a7d97a386c2997149d38804e399c78
dlsxjzld <sinjuk1@naver.com>
2024-12-02 16:39:12 +0900

    feat: 상품 정보 생성할 수 있도록 ProductList 모델 연결

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { PRODUCTS, PROMOTIONS, READ_ITEM_MESSAGE } from './constants.js';
+import ProductList from './model/ProductList.js';
 import { readItem } from './validation/validateFunctions.js';
 import { InputView } from './view/InputView.js';
+import { OutputView } from './view/OutputView.js';
 
 class App {
   async run() {
     const products = InputView.readFileSync(PRODUCTS);
     const promotions = InputView.readFileSync(PROMOTIONS);
+    const userInput = await this.getUserInput();
 
-    const userInput = await InputView.readUserInput(
-      READ_ITEM_MESSAGE,
-      readItem,
-    );
+    const productList = new ProductList(products);
+    OutputView.printResult(productList.printProductList());
+  }
+
+  // console.log('promotions', promotions);
+  async getUserInput() {
+    return InputView.readUserInput(READ_ITEM_MESSAGE, readItem);
   }
 }
 
